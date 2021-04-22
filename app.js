06276ee9d0d351edb9e4c9911e8fc3719017b5b1
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2021-04-22 12:16:03 -0400

    add compression

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const app = express();
-const routes = require('./routes/site')
+const routes = require('./routes/site');
+const compression = require('compression');
 
 // set pug engine
 app.set('view engine', 'pug');
 
+// set compression
+app.use(compression());
+
 // set static assets
 app.use(express.static('./public'));
 
-app.use('/', routes)
+app.use('/', routes);
 
 app.listen(5000, () => {
     console.log('server is listening on port 5000...')
-})
+});
