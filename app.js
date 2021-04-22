080e250e43026a0a338763e57b96a90c8d3d8dc4
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2021-04-22 12:35:46 -0400

    change port value

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ app.use(express.static('./public'));
 
 app.use('/', routes);
 
-app.listen(5000, () => {
-    console.log('server is listening on port 5000...')
+app.listen(process.env.PORT || 5000, () => {
+    console.log('Listening on port ' + process.env.PORT);
 });
