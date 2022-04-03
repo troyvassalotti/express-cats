b95d2b678ff6c0b19745aab5d15c05ba3141036d
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2022-04-03 18:00:42 -0400

    upgrade packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,30 @@
 module.exports = {
-  purge: {
-    enabled: true,
     content: [
-    './views/**/*.pug'
-    ]
-  },
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    screens: {
-      'md': '768px',
-      'lg': '1280px',
+        './views/**/*.pug'
+    ],
+    theme: {
+        screens: {
+            'md': '768px',
+            'lg': '1280px',
+        },
+        minHeight: {
+            '0': '0',
+            '1/4': '25%',
+            '1/2': '50%',
+            '3/4': '75%',
+            'full': '100%',
+            'screen': '100vh'
+        }
     },
-    minHeight: {
-      '0': '0',
-      '1/4': '25%',
-      '1/2': '50%',
-      '3/4': '75%',
-      'full': '100%',
-      'screen': '100vh'
-     }
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-  corePlugins: {
-    float: false,
-    accessibility: false,
-    backdropBrightness: false,
-    backdropContrast: false,
-    backdropGrayscale: false,
-    backdropHueRotate: false,
-    borderCollapse: false,
-    boxDecorationBreak: false,
-    clear: false
-  }
+    corePlugins: {
+        float: false,
+        accessibility: false,
+        backdropBrightness: false,
+        backdropContrast: false,
+        backdropGrayscale: false,
+        backdropHueRotate: false,
+        borderCollapse: false,
+        boxDecorationBreak: false,
+        clear: false
+    }
 }
